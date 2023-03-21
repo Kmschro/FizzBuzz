6ed23571fadaced6ea6f2f44c6bca07554f25db8
Kmschro <klaus072004@gmail.com>
2023-03-21 19:44:32 -0400

    Added FizzBuzz Program

--- FizzBuzz.java
+++ FizzBuzz.java
@@ -1,4 +1,32 @@
 public class FizzBuzz
 {
-    
+    public static void main(String args[])
+    {
+        int counter = 1;
+        while (counter > 0 && counter < 101)
+        {
+            if (counter%5 == 0 && counter%3 == 0)
+            {
+                System.out.println("FizzBuzz");
+                counter++;
+            }
+            else if (counter%3 == 0)
+            {
+                System.out.println("Fizz");
+                counter++;
+            } 
+            else if (counter%5 == 0)
+            {
+                System.out.println("Buzz");
+                counter++;
+            }
+            else
+            {
+                System.out.println(counter);
+                counter++;
+            }
+            
+        }
+
+    }
 }
